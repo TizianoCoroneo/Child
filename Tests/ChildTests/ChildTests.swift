7b4eb88f649ae7bf2d72bb8d1c738578dbf501f4
Tiziano Coroneo <tizianocoroneo@me.com>
2017-12-19 14:43:00 +0100

    Updated tests (added big json test) and travis.

--- Tests/ChildTests/ChildTests.swift
+++ Tests/ChildTests/ChildTests.swift
@@ -12,9 +12,10 @@ class ChildTests: XCTestCase {
     
     var runner: Runner! = Runner()
     
-    var jsonPath: String! = nil
+    var smallJsonPath: String! = nil
+    var bigJsonPath: String! = nil
     
-    let jsonTest = """
+    let smallJsonTest1 = """
 
 {
     "result": [
@@ -43,30 +44,176 @@ class ChildTests: XCTestCase {
 
 """
     
+    let bigJsonTest = """
+
+{
+    "result": [
+        {
+            "id": 2,
+            "firstName": "Daan",
+            "lastName": "Heikens",
+            "description": null,
+            "subscriptionType": null,
+            "averageRating": 6,
+            "hourlyRate": 0,
+            "currency": {
+                "id": 1,
+                "name": "EUR",
+                "sign": "€"
+            },
+            "friendsInCommon": 4,
+            "location": {
+                "lat": 52.015411,
+                "lng": 4.321424
+            },
+            "currentProjects": 2,
+            "totalProjects": 3,
+            "skills": [
+                {
+                    "name": "PHP"
+                },
+                {
+                    "name": "HTML"
+                },
+                {
+                    "name": "JAVA"
+                },
+                {
+                    "name": "C#\r\n"
+                }
+            ],
+            "emailVerified": false,
+            "phoneVerified": false,
+            "responseTime": 1,
+            "contactStatus": 1,
+            "contacts": {
+                "id": 4,
+                "imgUrl": null,
+                "firstName": "test",
+                "lastName": "test_last_name"
+            },
+            "sharedContacts": {
+                "id": 4,
+                "imgUrl": null,
+                "firstName": "test",
+                "lastName": "test_last_name"
+            },
+            "teams": [],
+            "portfolio": [
+                {
+                    "id": "3",
+                    "picture": "9930385ecf6505fcf81576460935772b.jpg",
+                    "title": "Webdesign Fashion",
+                    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna",
+                    "websiteUrl": null,
+                    "fileUrl": "45d02a1d8b99f86b5ed57bdbdc69bf80.pdf",
+                    "userId": "2"
+                },
+                {
+                    "id": "4",
+                    "picture": "9be9fdddda68be9a9a163ef1e7752f00.jpg",
+                    "title": "UI Development",
+                    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna",
+                    "websiteUrl": null,
+                    "fileUrl": "a7545541850c51eeea70d0c13a67949f.pdf",
+                    "userId": "2"
+                }
+            ],
+            "pastProjects": {
+                "external": {
+                    "id": 1,
+                    "name": "Title",
+                    "summary": "Description",
+                    "start_year": "2017-11-05 00:00:00",
+                    "end_year": "2017-11-10 00:00:00",
+                    "deliverable_url": "d-tt.nl",
+                    "budget_type": 1,
+                    "budget_min": 1,
+                    "budget_max": 100000
+                },
+                "internal": {
+                    "id": 3,
+                    "name": "Programmer needed for website",
+                    "summary": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+                    "posted_at": "2017-11-16 08:40:51",
+                    "posted_by": {
+                        "id": 2,
+                        "first_name": "Daan",
+                        "last_name": "Heikens"
+                    },
+                    "deliverable_url": "d-tt.nl"
+                }
+            },
+            "feedback": [
+                {
+                    "freelancer": {
+                        "quality": 0,
+                        "communication": 0,
+                        "deadline": 0,
+                        "professionalism": 0
+                    }
+                },
+                {
+                    "employer": {
+                        "quality": 3,
+                        "communication": 3.5,
+                        "deadline": 2.5,
+                        "professionalism": 3,
+                        "terms_of_payment": 4.5,
+                        "project_description": 3
+                    }
+                }
+            ]
+        }
+    ]
+}
+
+"""
+    
     override func setUp() {
         super.setUp()
         
         let manager = FileManager.default
         
-        let jsonName = "TestJSONModel.json"
+        let smallJsonName = "TestJSONModel.json"
+        let bigJsonName = "TestJSONModel.json"
+        
         let jsonFolderPath: URL = URL(string: NSTemporaryDirectory())!
         
-        jsonPath = jsonFolderPath.appendingPathComponent(jsonName).absoluteString
+        smallJsonPath = jsonFolderPath.appendingPathComponent(smallJsonName).absoluteString
+        
+        bigJsonPath = jsonFolderPath.appendingPathComponent(bigJsonName).absoluteString
         
         if manager.fileExists(
-            atPath: jsonPath) {
-            guard manager.isWritableFile(atPath: jsonPath) else {
-                fatalError("File at \(jsonPath) is not writable")
+            atPath: smallJsonPath) {
+            guard manager.isWritableFile(atPath: smallJsonPath) else {
+                fatalError("File at \(smallJsonPath) is not writable")
+            }
+        }
+        
+        if manager.fileExists(
+            atPath: bigJsonPath) {
+            guard manager.isWritableFile(atPath: bigJsonPath) else {
+                fatalError("File at \(bigJsonPath) is not writable")
             }
         }
         
         guard manager.createFile(
-            atPath: jsonPath,
-            contents: jsonTest.data(using: .utf8)!,
+            atPath: smallJsonPath,
+            contents: smallJsonTest1.data(using: .utf8)!,
             attributes: nil)
             else {
                 
-                fatalError("Failed creation of file at: \(jsonPath)")
+                fatalError("Failed creation of file at: \(smallJsonPath)")
+        }
+        
+        
+        guard manager.createFile(
+            atPath: bigJsonPath,
+            contents: bigJsonTest.data(using: .utf8)!,
+            attributes: nil)
+            else {
+                fatalError("Failed creation of file at: \(bigJsonPath)")
         }
         
         runner = Runner()
@@ -85,7 +232,7 @@ class ChildTests: XCTestCase {
             try runner.run(
             ["child",
              "-i",
-             jsonPath],
+             smallJsonPath],
             diagnostic: { print($0) }) {
                 let out = XCTAttachment.init(string: $0)
                 out.lifetime = .keepAlways
@@ -105,7 +252,7 @@ class ChildTests: XCTestCase {
             try runner.run(
                 ["child",
                  "-i",
-                 jsonPath,
+                 smallJsonPath,
                  "--codable"],
                 diagnostic: { print($0) }) {
                     let out = XCTAttachment.init(string: $0)
@@ -126,12 +273,33 @@ class ChildTests: XCTestCase {
             try runner.run(
                 ["child",
                  "-i",
-                 jsonPath,
+                 smallJsonPath,
                  "--extended-codable"],
                 diagnostic: { print($0) }) {
                     let out = XCTAttachment.init(string: $0)
                     out.lifetime = .keepAlways
                     out.name = "Codable.txt"
+                    self.add(out)
+            }
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
+    func testBigJson() {
+        do {
+            print("\n\n-----\n\n")
+            defer { print("\n\n-----\n\n") }
+            
+            try runner.run(
+                ["child",
+                 "-i",
+                 bigJsonPath,
+                 "--extended-codable"],
+                diagnostic: { print($0) }) {
+                    let out = XCTAttachment.init(string: $0)
+                    out.lifetime = .keepAlways
+                    out.name = "Big JSON.txt"
                     self.add(out)
             }
         } catch {
